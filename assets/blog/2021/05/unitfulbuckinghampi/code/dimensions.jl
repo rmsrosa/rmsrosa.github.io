b9a057297decd0bb68798292051e8b53f0b0b560
Ricardo Rosa <rmsrosa@gmail.com>
2021-05-02 22:15:35 -0300

    add blog post UnitfulBuckinghamPi.jl

---
+++ assets/blog/2021/05/unitfulbuckinghampi/code/dimensions.jl
@@ -0,0 +1,6 @@
+# This file was generated, do not modify it. # hide
+@show "Dimension of ℓ: $(dimension(ℓ))"
+@show "Dimension of g: $(dimension(g))"
+@show "Dimension of m: $(dimension(m))"
+@show "Dimension of τ: $(dimension(τ))"
+@show "Dimension of θ: $(dimension(θ))"
---
+++ assets/blog/2021/05/unitfulbuckinghampi/code/pdmat.jl
@@ -0,0 +1,3 @@
+# This file was generated, do not modify it. # hide
+pdmat = UnitfulBuckinghamPi.parameterdimensionmatrix()
+show(stdout, "text/plain", pdmat) # hide
